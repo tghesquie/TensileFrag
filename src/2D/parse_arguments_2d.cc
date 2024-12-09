1976c44d9a6101b2a72583e5134d88c74de4a7fe
tghesquie <thibault.ghesquiere@gmail.com>
2024-12-09 16:48:17 +0100

    Update .gitignore, add material data file, and added base_paths for 2D materials and meshes

--- src/2D/parse_arguments_2d.cc
+++ src/2D/parse_arguments_2d.cc
@@ -1,4 +1,5 @@
 #include "parse_arguments_2d.hh"
+#include <filesystem>
 
 SimulationArguments ParseArguments2D::parse(int argc, char** argv) {
     auto parser = getDefaultParser("Run 2D simulation");
@@ -15,9 +16,17 @@ SimulationArguments ParseArguments2D::parse(int argc, char** argv) {
     parser->parse_args(argc, argv);
 
     SimulationArguments args;
-    args.material_file = parser->get<std::string>("--material_file");
-    args.mesh_file = parser->get<std::string>("--mesh_file");
+    std::string material_file = parser->get<std::string>("--material_file");
+    std::string mesh_file = parser->get<std::string>("--mesh_file");
+
+    // Add base paths for 2D
+    std::string base_material_path = "2D/material/";
+    std::string base_mesh_path = "2D/mesh/";
+
+    args.material_file = std::filesystem::path(base_material_path) / material_file;
+    args.mesh_file = std::filesystem::path(base_mesh_path) / mesh_file;
     args.safety_factor = parser->get<double>("--safety_factor");
-    args.contact = true; // Add a default value or retrieve from arguments
+    args.strain_rate = parser->get<double>("--strain_rate");
+
     return args; // Ensure the return of a valid SimulationArguments object 
 }
